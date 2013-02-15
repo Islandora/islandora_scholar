a9786aca568ffc42acb84d63e859939a453a9f31
DannyJoris <danny_joris@yahoo.co.uk>
2013-02-15 02:11:31 +0000

    renamed variable and modified template file

--- templates/SearchItem.tpl.php
+++ templates/SearchItem.tpl.php
@@ -1,9 +1,11 @@
+<?php if ($icons): ?>
+  <div class="scholar-icons">
+    <?php print $icons; ?>  
+  </div>
+<?php endif; ?>
 <div class="scholar_search_item">
   <h4 class="scholar_search_item_title"><?php print $title ?></h4>
   <div class="scholar_search_item_content">
     <?php print $citation; ?>
   </div>
-</div>
-<div class="scholar-icons">
-  <?php print $pdf_icon; ?>  
 </div>
