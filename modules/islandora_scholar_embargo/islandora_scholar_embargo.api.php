7db7ff2acd1c30d47492a504e30aa0e9559c314e
Nigel Banks <nigel.g.banks@gmail.com>
2013-02-22 05:02:04 +0100

    Use a hook for getting the user to notify.
    It was half implemented and the POLICY datastream wasn't getting
    updated correctly as a consequence.

---
+++ modules/islandora_scholar_embargo/islandora_scholar_embargo.api.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * This file documents all available hook functions to manipulate data.
+ */
+
+/**
+ * Get the list of users to notify about embargo events.
+ *
+ * @param FedoraObject $object
+ *   The object to display
+ *
+ * @return array
+ *   An array whose values are Drupal user objects.
+ */
+function hook_islandora_scholar_embargo_users_to_notify(AbstractFedoraObject $object) {
+}
