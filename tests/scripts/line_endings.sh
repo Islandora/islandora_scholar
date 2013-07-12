7e6b53176c81044c7045554e79b73aa529121c50
Adam Vessey <adam@discoverygarden.ca>
2013-07-12 13:57:44 +0000

    Fix a little derp in the logic...
    ...  Was only checking the exclusions that contain stuff... Blargh.

--- tests/scripts/line_endings.sh
+++ tests/scripts/line_endings.sh
@@ -17,7 +17,7 @@ FILES=`find -L $1 -name "*.info" -o -name "*.txt" -o -name "*.md"`
 echo "Testing for files with DOS line endings..."
 for FILE in $FILES
 do
-  if containsElement "$FILE" "${EXCLUDE[@]}"; then
+  if ! containsElement "$FILE" "${EXCLUDE[@]}"; then
     file $FILE | grep CRLF
     if [ $? == 0 ]
     then
