ec558b425e1c538d1ad301f48ce2eb572ffd3ab7
willtp87 <willtp87@hotmail.com>
2014-04-24 14:53:23 -0300

    quick format fix

--- modules/citeproc/lib/citeproc-php/CSL_Dateparser.php
+++ modules/citeproc/lib/citeproc-php/CSL_Dateparser.php
@@ -20,15 +20,15 @@ class CSL_DateParser {
     }
     return $instance;
   }
-  
+
   protected $jiy, $jiysplitter, $jiymatcher, $jmd, $jy, $jr, $rexdash,
     $rexdashslash, $rexslashdash, $seasonrexes, $mstrings, $use_array = TRUE,
     $monthguess, $dayguess, $msets, $mabbrevs, $mrexes, $csl_date_parts_all;
-  
+
   private function set_parts($date_parts) {
     $this->csl_date_parts_all = $date_parts;
   }
-  
+
   private function __construct() {
     $this->jiy = array(
       "\x{660E}\x{6CBB}" => 1867,
@@ -36,17 +36,17 @@ class CSL_DateParser {
       "\x{662D}\x{548C}" => 1925,
       "\x{5E73}\x{6210}" => 1988
     );
-    
+
     $jiymatchstring = array_keys($this->jiy);
     $jiymatchstring = implode('|', $jiymatchstring);
-    
+
     $this->jiysplitter = "/(?:". $jiymatchstring .")(?:[0-9]+)/u";
     $this->jiymatcher = "/(?:". $jiymatchstring .")(?:[0-9]+)/u"; //Global!
 
     $this->jmd = "/(\x{6708}|\x{5E74})/u"; //Global!
     $this->jy = "/\x{65E5}/u";
     $this->jr = "/\x{301c}/u"; //Global!
-    
+
     //%%NUMD%% and %%DATED%% just seem to be templates
     $yearlast = "(?:[?0-9]{1,2}%%NUMD%%){0,2}[?0-9]{4}(?![0-9])";
     $yearfirst = "[?0-9]{4}(?:%%NUMD%%[?0-9]{1,2}){0,2}(?![0-9])";
@@ -66,30 +66,30 @@ class CSL_DateParser {
       //FIXME:  Might have to push, instead of 'enqueueing'
       $this->seasonrexes[] = $season .'.*';
     }
-    
+
     $this->mstrings = 'january february march april may june july august september october november december spring summer fall winter spring summer';
     $this->mstrings = explode(' ', $this->mstrings);
-    
+
     $this->setOrderMonthDay();
   }
-  
-	
-	private function setOrderDayMonth() {
-		$this->monthguess = 1;
-		$this->dayguess = 0;
-	}
-	private function setOrderMonthDay() {
-		$this->monthguess = 0;
-		$this->dayguess = 1;
-	}
+
+
+  private function setOrderDayMonth() {
+    $this->monthguess = 1;
+    $this->dayguess = 0;
+  }
+  private function setOrderMonthDay() {
+    $this->monthguess = 0;
+    $this->dayguess = 1;
+  }
 
   private function resetMonths() {
-		$this->msets = array();
+    $this->msets = array();
     foreach ($this->mstrings as $mstring) {
       $this->msets[] = array($mstring);
     }
 
-		$this->mabbrevs = array();
+    $this->mabbrevs = array();
     foreach ($this->msets as $set) {
       $temp = array();
       foreach ($set as $j) {
@@ -97,13 +97,13 @@ class CSL_DateParser {
       }
       $this->mabbrevs[] = $temp;
     }
-    
-		$this->mrexes = array();
+
+    $this->mrexes = array();
     foreach ($this->mabbrevs as $i) {
       $this->mrexes[] = "(?:". implode('|', $i) .')';
     }
-	}
-  
+  }
+
   private function addMonths($lst) {
     if (is_string($lst)) {
       $lst = preg_split('/\s+/');
@@ -112,11 +112,11 @@ class CSL_DateParser {
       //TODO:  Throw some kinda error message...
       return;
     }
-    
+
     //$othermatch = array(); //Not used?
     //$thismatch = array(); //Not used?
     $mab =& $this->mabbrevs;
-    
+
     foreach ($lst as $key_i => $val_i) {
       $abbrevlen = FALSE;
       $skip = FALSE;
@@ -162,7 +162,7 @@ class CSL_DateParser {
         $mab[$key_i][] = substr($val_i, 0, $insert);
       }
     }
-    
+
     $this->mrexes = array();
     foreach ($mab as $val) {
       $this->mrexes[] = '(?:'. implode('|', $val) .')';
@@ -172,7 +172,7 @@ class CSL_DateParser {
   public function parse($txt) {
     $slash = $dash = FALSE;
     $range_delim = $date_delim = NULL;
-    
+
     if (preg_match($this->jmd, $txt) > 0) {
       $txt = preg_replace($this->jy, '', $txt);
       $txt = preg_replace($this->jmd, '-', $txt);
@@ -201,13 +201,13 @@ class CSL_DateParser {
         $lst[$key + 1] = $this->jiy[$lst[$key]] + intval($lst[$key + 1], 10);
         $lst[$key] = '';
       }
-      
+
       $txt = implode('', $lst);
       $txt = preg_replace('/\s*-\s*$/', '', $txt);
       $txt = preg_replace('/\s*-\s*\//', '/', $txt);
       $txt = preg_replace('/\.\s*$/', '', $txt);
       $txt = preg_replace('/\.(?! )/', '', $txt);
-      
+
       $slash = strpos($txt, '/');
       $dash = strpos($txt, '-');
     }
@@ -215,7 +215,7 @@ class CSL_DateParser {
     $number = $note = NULL;
     $thedate = array();
     $suff = '';
-    
+
     if (substr($txt, 0, 1) === '"' && substr($txt, -1) === '"') {
       $thedate['literal'] = substr($txt, 1, -1);
       return $thedate;
@@ -239,15 +239,15 @@ class CSL_DateParser {
       $date_delim = '-';
       $lst = preg_split($this->rexdash, $txt);
     }
-    
+
     $ret = array();
     foreach ($lst as $val) {
       $match = NULL;
       if (preg_match('/^\s*([\-\/]|[a-zA-Z]+|[\-~?0-9]+)\s*$/', $val, $match) > 0) {
-        $ret[] = $match[1]; 
+        $ret[] = $match[1];
       }
     }
-    
+
     $delim_pos = array_search($range_delim, $ret);
     $delims = array();
     $is_range = FALSE;
@@ -259,12 +259,12 @@ class CSL_DateParser {
     else {
       $delims[] = array(0, count($ret));
     }
-   
+
     foreach ($delims as $delim) {
       $date = array_slice($ret, $delim[0], $delim[1]);
       foreach ($date as $element) {
         $lc = strtolower($element); //element.toLocaleLowerCase...
-        
+
         if (strpos($element, $date_delim) !== FALSE) {
           //TODO
           $this->parseNumericDate($thedate, $date_delim, $suff, $element);
@@ -273,9 +273,9 @@ class CSL_DateParser {
         elseif (preg_match('/[0-9]{4}/', $element) > 0) {
           $thedate['year'. $suff] = preg_replace('/^0*/', '', $element);
         }
-        
+
         $breakme = FALSE;
-        
+
         foreach ($this->mrexes as $key => $mrex) {
           //element.toLocaleLowerCase.match($mrex)
           if (preg_match($mrex, $lc) > 0) {
@@ -289,20 +289,20 @@ class CSL_DateParser {
           elseif (preg_match('/^[0-9]+$/', $lc)) {
             $number = intval($element);
           }
-          
+
           if ($number && preg_match('/^bc/', $element) > 0) {
             $thedate['year'. $suff] = ''. ($number * -1);
-						$number = '';
-						continue;
+            $number = '';
+            continue;
           }
           elseif ($number && preg_match('/^ad/', $element) > 0) {
             $thedate['year'. $suff] = ''. $number;
-						$number = '';
-						continue;
+            $number = '';
+            continue;
           }
         }
         $breakme = FALSE;
-        
+
         foreach ($this->seasonrexes as $key => $srex) {
           if (preg_match($srex, $lc) > 0) {
             $thedate['season'. $suff] = ''. ($key + 1);
@@ -346,7 +346,7 @@ class CSL_DateParser {
     if (!in_array('year', $thedate)) {
       $thedate = array('literal' => $txt);
     }
-    
+
     if ($this->use_array) {
       return $this->toArray($thedate);
     }
@@ -354,19 +354,19 @@ class CSL_DateParser {
       return $thedate;
     }
   }
-  
+
   public function returnAsArray() {
     $this->use_array = TRUE;
   }
-  
+
   public function returnAsKeys() {
     $this->use_array = FALSE;
   }
-  
+
   private function toArray($thedate) {
     //dd($thedate, 'toArray');
     $toReturn = array('date-parts' => array());
-    
+
     if (array_key_exists('literal', $thedate)) {
       $toReturn = array('literal' => $thedate['literal']);
     }
@@ -394,12 +394,12 @@ class CSL_DateParser {
       $toReturn['date-parts'][] = $start;
       $toReturn['date-parts'][] = $end;
     }
-    
+
     //dd($toReturn, 'Fully parsed stuff...');
     //dd($this, 'Parser object');
     return $toReturn;
   }
-	
+
   private function parseNumericDate(&$ret, $delim, $suff, $txt) {
     $lst = preg_split($delim, $txt);
     foreach ($lst as $key => $val) {
@@ -417,19 +417,19 @@ class CSL_DateParser {
     foreach ($lst as &$val) {
       $val = intval($val);
     }
-    
+
     if (count($lst) === 1) {
       $ret['month'. $suff] = ''. $lst[0];
     }
     elseif (count($lst) === 2) {
       if ($lst[$this->monthguess] > 12) { //FIXME:  This doesn't really work...
-				$ret['month'. $suff] = ''. $lst[$this->dayguess];
-				$ret['day'. $suff] = ''. $lst[$this->monthguess];
-			} 
+        $ret['month'. $suff] = ''. $lst[$this->dayguess];
+        $ret['day'. $suff] = ''. $lst[$this->monthguess];
+      }
       else {
-				$ret['month'. $suff] = ''. $lst[$this->monthguess];
-				$ret['day'. $suff] = ''. $lst[$this->dayguess];
-			}
+        $ret['month'. $suff] = ''. $lst[$this->monthguess];
+        $ret['day'. $suff] = ''. $lst[$this->dayguess];
+      }
     }
   }
 }
