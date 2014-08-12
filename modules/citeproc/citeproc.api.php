03e4d82c229a9cce80dbeafb9fcb14470c69d6a1
Adam Vessey <adam@discoverygarden.ca>
2014-08-12 10:57:33 -0300

    Add example implementation.

--- modules/citeproc/citeproc.api.php
+++ modules/citeproc/citeproc.api.php
@@ -14,5 +14,21 @@
  *   The MODS from which the original output was produced.
  */
 function hook_convert_mods_to_citeproc_jsons_alter(array &$output, SimpleXMLElement $mods) {
+  if (!empty($output['abstract'])) {
+    $texts = array();
 
+    // CSL can't build up a "structured" abstract on its own, as cannot accept
+    // multiple abstracts with labels... So something like this has been
+    // desirable elsewhere.
+    foreach ($mods->xpath('//mods:mods[1]/mods:abstract') as $abstract) {
+      $text = isset($abstract['displayLabel']) ?
+        trim($abstract['displayLabel']) . ': ':
+        '';
+
+      $text .= (string) $abstract;
+      $texts[] = $text;
+    }
+
+    $output['abstract'] = implode(' ', $texts);
+  }
 }
